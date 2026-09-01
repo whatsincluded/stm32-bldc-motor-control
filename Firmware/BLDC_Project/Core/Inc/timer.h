62db55cd6de012b3df6fab835bedbefa54e21457
skateboard-fluke <kimdongkyu2416@gmail.com>
2026-09-01 15:55:12 +0900

    initial commit

---
+++ Firmware/BLDC_Project/Core/Inc/timer.h
@@ -0,0 +1,14 @@
+/*
+ * timer.h
+ *
+ *  Created on: Aug 31, 2026
+ *      Author: kdk78
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+
+
+
+#endif /* INC_TIMER_H_ */
---
+++ Firmware/BLDC_Project/Core/Src/timer.c
@@ -0,0 +1,15 @@
+/*
+ * timer.c
+ *
+ *  Created on: Aug 31, 2026
+ *      Author: kdk78
+ */
+
+
+#include "timer.h"
+
+
+void Initialize_PWM(void)
+{
+
+}
